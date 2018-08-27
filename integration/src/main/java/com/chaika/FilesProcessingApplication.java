8bf2992cd4db5acbe0315bd56fb6a26bcab2c3b8
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2018-08-27 18:15:04 +0300

    Initial commit

---
+++ integration/src/main/java/com/chaika/FilesProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.chaika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FilesProcessingApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesProcessingApplication.class, args);
+    }
+}
